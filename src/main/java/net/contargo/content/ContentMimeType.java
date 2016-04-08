960e9e3a139f6246dcf9fbc724d477fcb986120f
Olle Törnström <toernstroem@synyx.de>
2016-04-08 21:03:31 +0200

    Add mime-type javadocs

--- src/main/java/net/contargo/content/ContentMimeType.java
+++ src/main/java/net/contargo/content/ContentMimeType.java
@@ -3,6 +3,24 @@ package net.contargo.content;
 /**
  * Provides the declared constant content mime-types.
  *
+ * <p>Our use of a {@code mimeType} property, is how we like to classify and structure content. This class is an attempt
+ * to define the currently known and supported declarations.</p>
+ *
+ * <p>Though not strictly IANA or RFC compatible, we have chosen to keep to a structure that expresses <i>some
+ * compatibility</i> (and future safety) with said standards. Please note that it's currently also <strong>not</strong>
+ * the intent to publish these type definitions, outside of the COLA system landscape.</p>
+ *
+ * <p>Our mime type definitions are defined using the following pattern, with optional variations:</p>
+ *
+ * <pre><code>
+${type}/vnd.contargo.${name}
+${type}/vnd.contargo.${name};charset=${charset}
+${type}/vnd.contargo.${name}+${subtype}
+${type}/vnd.contargo.${name}+${subtype};charset=${charset}
+   </code></pre>
+ *
+ * <p>The constant {@code vnd.contargo} is our vendor specifier.</p>
+ *
  * @author  Olle Törnström - toernstroem@synyx.de
  * @since  0.1
  */
